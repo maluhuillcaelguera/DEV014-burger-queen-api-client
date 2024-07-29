6aeba64174fcc2098eccbbbfa3ed80d3b015e333
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-07-29 09:57:59 -0500

    Implement Chef component with a welcome message and placeholder for content.

---
+++ src/Componentes/Chef.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chef: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenido, Cocinero</h1>
+      {/* Aquí puedes agregar el contenido específico para la vista de cocinero */}
+    </div>
+  );
+};
+
+export default Chef;
